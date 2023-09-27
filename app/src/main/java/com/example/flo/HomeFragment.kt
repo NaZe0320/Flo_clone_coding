1a49f6e7e87baee2be9d9f1fe6a53dc5e2f936ea
NaZe0320 <rwt1226@naver.com>
2023-09-27 17:31:57 +0900

    [add] 1주차 미션 수행
    - 완료

--- app/src/main/java/com/example/flo/HomeFragment.kt
+++ app/src/main/java/com/example/flo/HomeFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.HomeAdapter
 import com.example.flo.databinding.FragmentHomeBinding
+import com.example.model.ItemData
 
 class HomeFragment : Fragment() {
 
@@ -17,6 +19,13 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val albumAdapter = HomeAdapter(1)
+        val potCastAdapter = HomeAdapter(2)
+        val videoAdapter = HomeAdapter(3)
+
+        binding.rvHomeMusic.adapter = albumAdapter
+        binding.rvHomePotcast.adapter = potCastAdapter
+        binding.rvHomeVideo.adapter = videoAdapter
 
         return binding.root
     }
---
+++ app/src/main/java/com/example/model/ItemData.kt
@@ -0,0 +1,8 @@
+package com.example.model
+
+data class ItemData (
+    val id: Int,
+    val title: String? = "제목",
+    val singer: String? = "가수"
+){
+}
